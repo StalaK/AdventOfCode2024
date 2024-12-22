7a56870af8eb80aada063303ed7cee039c4d3e94
joshhughes <stalak@gmail.com>
2024-12-22 20:40:37 +0000

    head empty

--- AdventOfCode2024/Day13.cs
+++ AdventOfCode2024/Day13.cs
@@ -109,7 +109,7 @@ internal static class Day13
     {
         const int A_COST = 3;
         const int B_COST = 1;
-        const long MISCALC = 10000000000000;
+        const long MISCALC = 0;
 
         var tokenCount = 0;
 
@@ -142,91 +142,21 @@ internal static class Day13
 
             (long x, long y) prize = (long.Parse(x) + MISCALC, long.Parse(y) + MISCALC);
 
-            Queue<(int aPress, int bPress, int xTotal, int yTotal)> moveQueue = [];
-            moveQueue.Enqueue((0, 0, 0, 0));
+            long xa = aButton.x * bButton.y;
+            long xb = bButton.x * aButton.y;
+            long xdiff = xa - xb;
+            //https://www.reddit.com/r/adventofcode/comments/1hd5b6o/comment/m1ub740/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+            System.Console.WriteLine("" + xdiff);
 
-            Dictionary<(int, int), bool> visitedStates = [];
-            visitedStates.Add((0, 0), true);
+            long pa = prize.x * bButton.y;
+            long pb = prize.x * aButton.y;
+            long pdiff = pa - pb;
 
-            var minTokens = int.MaxValue;
+            long o = pdiff / xdiff;
 
-            while (moveQueue.Count > 0)
-            {
-                var (aPress, bPress, xTotal, yTotal) = moveQueue.Dequeue();
-
-                // a button
-                var aPressCount = TimesCanPressButton(visitedStates, aPress, bPress, xTotal, yTotal, aButton, prize, 10_000);
-
-                if (aPressCount > 0 && !visitedStates.ContainsKey((aPress + aPressCount, bPress)))
-                {
-                    visitedStates.Add((aPress + aPressCount, bPress), true);
-
-                    if (xTotal + aButton.x == prize.x && yTotal + aButton.y == prize.y)
-                    {
-                        var thisMin = ((aPress + aPressCount) * A_COST) + (bPress * B_COST);
-                        minTokens = thisMin < minTokens ? thisMin : minTokens;
-                    }
-
-                    if (xTotal + aButton.x < prize.x && yTotal + aButton.y < prize.y)
-                        moveQueue.Enqueue((aPress + aPressCount, bPress, xTotal + aButton.x, yTotal + aButton.y));
-                }
-
-                // b button
-                var bPressCount = TimesCanPressButton(visitedStates, aPress, bPress, xTotal, yTotal, bButton, prize, 10_000);
-
-                if (bPressCount > 0 && !visitedStates.ContainsKey((aPress, bPress + bPressCount)))
-                {
-                    visitedStates.Add((aPress, bPress + bPressCount), true);
-
-                    if (xTotal + bButton.x == prize.x && yTotal + bButton.y == prize.y)
-                    {
-                        var thisMin = (aPress * A_COST) + ((bPress + bPressCount) * B_COST);
-                        minTokens = thisMin < minTokens ? thisMin : minTokens;
-                    }
-
-                    if (xTotal + bButton.x < prize.x && yTotal + bButton.y < prize.y)
-                        moveQueue.Enqueue((aPress, bPress + bPressCount, xTotal + bButton.x, yTotal + bButton.y));
-                }
-            }
-
-            if (minTokens != int.MaxValue)
-                tokenCount += minTokens;
+            System.Console.WriteLine(o);
         }
 
         Console.WriteLine($"Part 2: {tokenCount}");
-    }
-
-    private static int TimesCanPressButton(
-        Dictionary<(int, int), bool> visitedStates,
-        int aPress,
-        int bPress,
-        long xTotal,
-        long yTotal,
-        (int x, int y) button,
-        (long x, long y) prize,
-        int initialStart)
-    {
-        if (initialStart == 0)
-            return initialStart;
-
-        var canPress = true;
-
-        if (visitedStates.ContainsKey((aPress + initialStart, bPress)))
-            canPress = false;
-
-        if (xTotal + (button.x * initialStart) >= prize.x || yTotal + (button.y * initialStart) >= prize.y)
-            canPress = false;
-
-        return canPress
-            ? initialStart
-            : TimesCanPressButton(
-                visitedStates,
-                aPress,
-                bPress,
-                xTotal,
-                yTotal,
-                button,
-                prize,
-                (int)Math.Round((double)(initialStart / 2), 0, MidpointRounding.ToZero));
     }
 }
