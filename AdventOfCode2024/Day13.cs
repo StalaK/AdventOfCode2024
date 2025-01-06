cb72401272b084153707bdadddec80de289e4825
Josh Hughes <stalak@gmail.com>
2025-01-06 17:39:48 +0000

    Day 13 finished

--- AdventOfCode2024/Day13.cs
+++ AdventOfCode2024/Day13.cs
@@ -109,54 +109,90 @@ internal static class Day13
     {
         const int A_COST = 3;
         const int B_COST = 1;
-        const long MISCALC = 0;
+        const long MISCALC = 10000000000000;
 
-        var tokenCount = 0;
+        var tokenCount = 0L;
 
         for (int i = 0; i < machines.Count; i += 3)
         {
-            var xStart = machines[i].IndexOf("X+") + 2;
-            var xLength = machines[i].IndexOf(',') - xStart;
-            var x = machines[i].Substring(xStart, xLength);
+            var aButton = GetAButton(machines, i);
+            var bButton = GetBButton(machines, i);
+            var prize = GetPrizeLocation(machines, i, MISCALC);
 
-            var yStart = machines[i].IndexOf("Y+") + 2;
-            var y = machines[i][yStart..];
+            var aButtonPresses = GetAButtonPresses(aButton, bButton, prize);
 
-            (int x, int y) aButton = (int.Parse(x), int.Parse(y));
+            if (aButtonPresses < 0)
+                continue;
 
-            xStart = machines[i + 1].IndexOf("X+") + 2;
-            xLength = machines[i + 1].IndexOf(',') - xStart;
-            x = machines[i + 1].Substring(xStart, xLength);
+            var bButtonPresses = GetBButtonPresses(aButton, bButton, prize, aButtonPresses);
 
-            yStart = machines[i + 1].IndexOf("Y+") + 2;
-            y = machines[i + 1][yStart..];
+            if (bButtonPresses < 0)
+                continue;
 
-            (int x, int y) bButton = (int.Parse(x), int.Parse(y));
-
-            xStart = machines[i + 2].IndexOf("X=") + 2;
-            xLength = machines[i + 2].IndexOf(',') - xStart;
-            x = machines[i + 2].Substring(xStart, xLength);
-
-            yStart = machines[i + 2].IndexOf("Y=") + 2;
-            y = machines[i + 2][yStart..];
-
-            (long x, long y) prize = (long.Parse(x) + MISCALC, long.Parse(y) + MISCALC);
-
-            long xa = aButton.x * bButton.y;
-            long xb = bButton.x * aButton.y;
-            long xdiff = xa - xb;
-            //https://www.reddit.com/r/adventofcode/comments/1hd5b6o/comment/m1ub740/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-            System.Console.WriteLine("" + xdiff);
-
-            long pa = prize.x * bButton.y;
-            long pb = prize.x * aButton.y;
-            long pdiff = pa - pb;
-
-            long o = pdiff / xdiff;
-
-            System.Console.WriteLine(o);
+            tokenCount += (aButtonPresses * A_COST) + (bButtonPresses * B_COST);
         }
 
         Console.WriteLine($"Part 2: {tokenCount}");
     }
+
+    private static Button GetAButton(List<string> machines, int i)
+    {
+        var xStart = machines[i].IndexOf("X+") + 2;
+        var xLength = machines[i].IndexOf(',') - xStart;
+        var x = machines[i].Substring(xStart, xLength);
+        var yStart = machines[i].IndexOf("Y+") + 2;
+        var y = machines[i][yStart..];
+
+        return new Button(int.Parse(x), int.Parse(y));
+    }
+
+    private static Button GetBButton(List<string> machines, int i)
+    {
+        var xStart = machines[i + 1].IndexOf("X+") + 2;
+        var xLength = machines[i + 1].IndexOf(',') - xStart;
+        var x = machines[i + 1].Substring(xStart, xLength);
+        var yStart = machines[i + 1].IndexOf("Y+") + 2;
+        var y = machines[i + 1][yStart..];
+
+        return new Button(int.Parse(x), int.Parse(y));
+    }
+
+    private static Prize GetPrizeLocation(List<string> machines, int i, long miscalc)
+    {
+        var xStart = machines[i + 2].IndexOf("X=") + 2;
+        var xLength = machines[i + 2].IndexOf(',') - xStart;
+        var x = machines[i + 2].Substring(xStart, xLength);
+        var yStart = machines[i + 2].IndexOf("Y=") + 2;
+        var y = machines[i + 2][yStart..];
+
+        return new Prize(long.Parse(x) + miscalc, long.Parse(y) + miscalc);
+    }
+
+    private static long GetAButtonPresses(Button aButton, Button bButton, Prize prize)
+    {
+        long xa = (aButton.x * bButton.y) + (bButton.x * bButton.y);
+        long xprize = prize.x * bButton.y;
+
+        long ya = (aButton.y * bButton.x) + (bButton.y * bButton.x);
+        long yprize = prize.y * bButton.x;
+
+        long t = xa - ya;
+        long p = xprize - yprize;
+
+        var ans = p / t;
+        return p % t == 0
+            ? ans
+            : -1;
+    }
+
+    private static long GetBButtonPresses(Button aButton, Button bButton, Prize prize, long aPresses)
+    {
+        var b = (prize.x - (aButton.x * aPresses));
+        return b % bButton.x == 0
+            ? b / bButton.x
+            : -1;
+    }
+
+    private record Button(int x, int y);
+    private record Prize(long x, long y);
 }
